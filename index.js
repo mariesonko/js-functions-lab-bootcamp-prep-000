4fbefd79ec6a13caa825e305fc95b03eec0e7b9a
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-06 13:54:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function happyHolidaysTo(name){
   return (`Happy holidays, ${name}!`)
 }
 function happyHolidayTo(holiday,name){
-  return (`Happy ${holidays}, ${name}!`)
+  return (`Happy ${holiday}, ${name}!`)
 }
